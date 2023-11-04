16187a52cc4165a5bb2949c7d16f3376ce3f02ba
NicoLoreto <nicoloreto.dev@gmail.com>
2023-11-04 19:21:34 -0300

    folder restructuring

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+};
